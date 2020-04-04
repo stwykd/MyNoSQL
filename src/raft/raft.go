17c39baeb0460198b39580758eb445e6003a7a64
stwykd <5012027+stwykd@users.noreply.github.com>
2020-04-04 12:29:56 +0100

    Refactor Raft struct

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -9,22 +9,22 @@ import (
 // Raft Consensus Algorithm implemented as a library
 // https://pdos.csail.mit.edu/6.824/papers/raft-extended.pdf
 
-// State for a single Raft server
-type State struct {
-	me int // id of this Raft server
-	leader int // id of leader
-	cluster []*rpc.Server // RPC endpoint of each server in Raft cluster (including this one)
-	mu sync.Mutex // mutex to protect shared access and ensure visibility
-	electionTimer *time.Timer // time to wait before starting election
-	role Role // role of this Raft server
-	doneCh chan DoneMsg // communicate back to Raft client when a command is committed into log
-	logIndex int // log index where to store next log entry
+// Raft for a single Raft server
+type Raft struct {
+	me            int           // id of this Raft server
+	leader        int           // id of leader
+	cluster       []*rpc.Client // RPC endpoint of each server in Raft cluster (including this one)
+	mu            sync.Mutex    // mutex to protect shared access and ensure visibility
+	electionTimer *time.Timer   // time to wait before starting election
+	role          Role          // role of this Raft server
+	doneCh        chan DoneMsg  // communicate back to Raft client when a command is committed into log
+	logIndex      int           // log index where to store next log entry
 
 	// State from Figure 2 of Raft paper
 	// Persistent state on all servers
-	currentTerm int // latest term server has seen (initialized to 0 on first boot, increases monotonically)
-	votedFor int // candidateId that received vote in current term (or null if none)
-	log []LogEntry // each entry contains command for state machine, and term when entry was received by leader (first index is 1)
+	currentTerm int        // latest term server has seen (initialized to 0 on first boot, increases monotonically)
+	votedFor    int        // candidateId that received vote in current term (or null if none)
+	log         []LogEntry // each entry contains command for state machine, and term when entry was received by leader (first index is 1)
 
 	// Volatile state on all servers
 	commitIndex int // index of highest log entry known to be committed (initialized to 0, increases monotonically)
@@ -32,26 +32,24 @@ type State struct {
 
 	// Volatile state on leaders
 	// Reinitialized after election
-	nextIndex []int // for each server, index of the next log entry to send to that server (initialized to leader last log index + 1)
+	nextIndex  []int // for each server, index of the next log entry to send to that server (initialized to leader last log index + 1)
 	matchIndex []int //for each server, index of highest log entry known to be replicated on server (initialized to 0, increases monotonically)
 }
 
-
-
 // New initializes a new Raft server as of Figure 2 of Raft paper
-func New(cluster []*rpc.Server, me int, doneCh chan DoneMsg) *State {
-	s := &State{}
-	s.cluster = cluster
-	s.me = me
-	s.leader = -1
-	s.currentTerm = 0
-	s.votedFor = -1
-	s.commitIndex = 0
-	s.lastApplied = 0
-	s.role = Follower // all nodes start as follower
-	s.log = []LogEntry{{0, 0, nil}} // log entry at index 0 is unused
-	s.logIndex = 1
-	s.doneCh = doneCh
-	s.electionTimer = randElectionTimer()
-	return s
+func New(cluster []*rpc.Client, me int, doneCh chan DoneMsg) *Raft {
+	r := &Raft{}
+	r.cluster = cluster
+	r.me = me
+	r.leader = -1
+	r.currentTerm = 0
+	r.votedFor = -1
+	r.commitIndex = 0
+	r.lastApplied = 0
+	r.role = Follower               // all nodes start as follower
+	r.log = []LogEntry{{0, 0, nil}} // log entry at index 0 is unused
+	r.logIndex = 1
+	r.doneCh = doneCh
+	r.electionTimer = randElectionTimer()
+	return r
 }
