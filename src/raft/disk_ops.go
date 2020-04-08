66b53d8002522d3399527c23b10180a02ada28ed
stwykd <5012027+stwykd@users.noreply.github.com>
2020-04-08 08:43:54 +0100

    Refactor disk_ops.go

--- src/raft/disk_ops.go
+++ src/raft/disk_ops.go
@@ -12,7 +12,6 @@ func (r *Raft) encode() []byte {
 	e := gob.NewEncoder(w)
 	if e.Encode(r.currentTerm) != nil ||
 		e.Encode(r.votedFor) != nil ||
-		e.Encode(r.snapshotIdx) != nil ||
 		e.Encode(r.logIndex) != nil ||
 		e.Encode(r.commitIndex) != nil ||
 		e.Encode(r.lastApplied) != nil ||
@@ -29,17 +28,17 @@ func (r *Raft) decode(data []byte) {
 		return
 	}
 	d := gob.NewDecoder(bytes.NewBuffer(data))
-	currentTerm, votedFor, lastIncludedIndex, logIndex, commitIndex, lastApplied := 0, 0, 0, 0, 0, 0
+	currentTerm, votedFor, logIndex, commitIndex, lastApplied := 0, 0, 0, 0, 0
 	if d.Decode(&currentTerm) != nil ||
 		d.Decode(&votedFor) != nil ||
-		d.Decode(&lastIncludedIndex) != nil ||
 		d.Decode(&logIndex) != nil ||
 		d.Decode(&commitIndex) != nil ||
 		d.Decode(&lastApplied) != nil ||
 		d.Decode(&r.log) != nil {
 		log.Fatal("error while unmarshaling raft state")
 	}
-	r.currentTerm, r.votedFor, r.snapshotIdx, r.logIndex, r.commitIndex, r.lastApplied = currentTerm, votedFor, lastIncludedIndex, logIndex, commitIndex, lastApplied
+	r.currentTerm, r.votedFor, r.logIndex, r.commitIndex, r.lastApplied =
+		currentTerm, votedFor, logIndex, commitIndex, lastApplied
 }
 
 func (r *Raft) saveState() {
