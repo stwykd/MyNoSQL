3d9b44f83e471807881b5125417790c7c9511867
stwykd <5012027+stwykd@users.noreply.github.com>
2020-04-08 11:39:52 +0100

    Refactor raft.go

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -16,11 +16,8 @@ type Raft struct {
 	cluster     []*rpc.Client // RPC endpoint of each server in Raft cluster (including this one)
 	mu          sync.Mutex    // mutex to protect shared access and ensure visibility
 	timer       *time.Timer   // time to wait before starting election
-	role        Role          // role of this Raft server
-	doneCh      chan DoneMsg  // communicate back to Raft client when a command is committed into log
+	state       State         // state of this Raft server
 	logIndex    int           // log index where to store next log entry
-	snapshotIdx int           // index of the last entry within log replaced by snapshot, initialized to 0
-	notifyCh    chan struct{} // notify Raft client
 
 	// State from Figure 2 of Raft paper
 	// Persistent state on all servers
@@ -38,28 +35,6 @@ type Raft struct {
 	matchIndex   []int //for each server, index of highest log entry known to be replicated on server (initialized to 0, increases monotonically)
 }
 
-func (r *Raft) apply() {
-	for {
-		select {
-		case <-r.notifyCh:
-			r.mu.Lock()
-			var entries []LogEntry
-			if r.lastApplied < r.snapshotIdx {
-				r.lastApplied = r.snapshotIdx
-				entries = []LogEntry{{Index: r.snapshotIdx, Term: r.log[0].Term, Command: "InstallSnapshot"}}
-			} else if r.lastApplied < r.logIndex && r.lastApplied < r.commitIndex {
-				entries = append([]LogEntry{}, r.log[r.lastApplied+1- r.snapshotIdx:r.commitIndex+1- r.snapshotIdx]...)
-				r.lastApplied = r.commitIndex
-			}
-			r.saveState()
-			r.mu.Unlock()
-			for _, entry := range entries {
-				r.doneCh <- DoneMsg{Index: entry.Index, Term: entry.Term, Command: entry.Command}
-			}
-		}
-	}
-}
-
 // New initializes a new Raft server as of Figure 2 of Raft paper
 func New(cluster []*rpc.Client, me int, doneCh chan DoneMsg) *Raft {
 	r := &Raft{}
@@ -70,16 +45,23 @@ func New(cluster []*rpc.Client, me int, doneCh chan DoneMsg) *Raft {
 	r.votedFor = -1
 	r.commitIndex = 0
 	r.lastApplied = 0
-	r.role = Follower // all nodes start as follower
+	r.state = Follower              // all nodes start as follower
 	r.log = []LogEntry{{0, 0, nil}} // log entry at index 0 is unused
 	r.logIndex = 1
-	r.doneCh = doneCh
-	r.notifyCh = make(chan struct{}, 100)
-	r.snapshotIdx = 0
 	r.timer = randElectionTimer()
 
 	r.loadState() // initialize from saved state before possible server crash
-	go r.startTimer()
+	go r.runElectionTimer()
 
 	return r
+}
+
+// toFollower turns Raft server into follower
+// Expects r.mu to be acquired
+func (r *Raft) toFollower(term int) {
+	r.state = Follower
+	r.currentTerm = term
+	r.votedFor = -1
+
+	go r.runElectionTimer()
 }
