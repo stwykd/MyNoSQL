664975c45f37ee78bb7b6b8d5346191d0ee60f35
stwykd <5012027+stwykd@users.noreply.github.com>
2020-04-12 12:54:40 +0100

    implement follower wait behaviour before starting election

--- src/raft/election.go
+++ src/raft/election.go
@@ -6,9 +6,39 @@ import (
 )
 
 
-const minElectionTimer, maxElectionTimer = 150, 300
+const minElectionWait, maxElectionWait = 150, 300
 
-// generates a random duration between 150ms and 300 ms
-func randElectionTimer() *time.Timer {
-	return time.NewTimer(time.Duration(rand.Intn(maxElectionTimer)-minElectionTimer))
+// electionWait starts a timer towards becoming a candidate in a new election.
+// it is run by a follower as long as it receives heartbeats
+func (rf *Raft) electionWait() {
+	waitTimeout := time.Duration(rand.Intn(maxElectionWait)-minElectionWait) * time.Millisecond
+	rf.mu.Lock()
+	termStarted := rf.currentTerm
+	rf.mu.Unlock()
+
+	ticker := time.NewTicker(10 * time.Millisecond)
+	defer ticker.Stop()
+	for {
+		<-ticker.C
+
+		rf.mu.Lock()
+		if rf.state != Candidate && rf.state != Follower {
+			rf.mu.Unlock()
+			return
+		}
+
+		if termStarted != rf.currentTerm {
+			rf.mu.Unlock()
+			return
+		}
+
+		// Start an election if we haven't heard from a leader or haven't voted for
+		// someone for the duration of the timeout.
+		if elapsed := time.Since(rf.resetElection); elapsed >= waitTimeout {
+			//rf.election()
+			rf.mu.Unlock()
+			return
+		}
+		rf.mu.Unlock()
+	}
 }
