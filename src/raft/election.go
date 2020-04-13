c60daba4ef38e9c782e3993b23093d466b3fdcaf
stwykd <5012027+stwykd@users.noreply.github.com>
2020-04-13 08:52:43 +0100

    implement election

--- src/raft/election.go
+++ src/raft/election.go
@@ -2,6 +2,8 @@ package raft
 
 import (
 	"math/rand"
+	"net/rpc"
+	"sync/atomic"
 	"time"
 )
 
@@ -35,10 +37,56 @@ func (rf *Raft) electionWait() {
 		// Start an election if we haven't heard from a leader or haven't voted for
 		// someone for the duration of the timeout.
 		if elapsed := time.Since(rf.resetElection); elapsed >= waitTimeout {
-			//rf.election()
+			rf.election()
 			rf.mu.Unlock()
 			return
 		}
 		rf.mu.Unlock()
 	}
+}
+
+// election starts a new election with this server as a candidate.
+// Expects rf.mu to be locked.
+func (rf *Raft) election() {
+	rf.state = Candidate
+	rf.currentTerm++
+	savedCurrentTerm := rf.currentTerm
+	rf.resetElection = time.Now()
+	rf.votedFor = rf.me
+
+	var votes int32 = 1
+	for _, peer := range rf.cluster {
+		go func(peer *rpc.Client) {
+			args := RequestVoteArgs{
+				Term:        savedCurrentTerm,
+				CandidateId: rf.me,
+			}
+
+			var reply RequestVoteReply
+			if err := peer.Call("Raft.RequestVote", args, &reply); err == nil {
+				rf.mu.Lock()
+				defer rf.mu.Unlock()
+
+				if rf.state != Candidate {
+					return
+				}
+
+				if reply.Term > savedCurrentTerm {
+					rf.toFollower(reply.Term)
+					return
+				} else if reply.Term == savedCurrentTerm {
+					if reply.VoteGranted {
+						votes := int(atomic.AddInt32(&votes, 1))
+						if votes*2 > len(rf.cluster)+1 { // election won
+							rf.leader()
+							return
+						}
+					}
+				}
+			}
+		}(peer)
+	}
+
+	// this election didn't produce a leader, call electionWait again
+	go rf.electionWait()
 }
