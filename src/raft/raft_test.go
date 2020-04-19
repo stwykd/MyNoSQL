134ab26661fa1c1f640ef56ef68d0c3ad5761aef
stwykd <5012027+stwykd@users.noreply.github.com>
2020-04-19 20:29:01 +0100

    Add helper functions for testing

--- src/raft/raft_test.go
+++ src/raft/raft_test.go
@@ -1,2 +1,31 @@
 package raft
 
+import (
+	"fmt"
+	//"github.com/golang/mock/gomock"
+	"log"
+	"net"
+	"net/rpc"
+)
+
+const ClusterSize = 5
+
+var raftServers []*Raft
+
+func getPeers(cluster map[int]*rpc.Client, me int) map[int]*rpc.Client {
+	peers := make(map[int]*rpc.Client, ClusterSize-1)
+	for i, p := range cluster {
+		if i != me {
+			peers[i] = p
+		}
+	}
+	return peers
+}
+
+func listenTCP(port int) (net.Listener, string) {
+	l, e := net.Listen("tcp", fmt.Sprintf("127.0.0.1:%d", port))
+	if e != nil {
+		log.Fatalf("net.Listen tcp :%d: %v", port, e)
+	}
+	return l, l.Addr().String()
+}
