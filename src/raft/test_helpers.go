3000bfb0f7409fe6c8cca1f95994489d43e4f3a2
stwykd <5012027+stwykd@users.noreply.github.com>
2020-05-04 15:23:38 +0100

    disconnect raft servers for testing

--- src/raft/test_helpers.go
+++ src/raft/test_helpers.go
@@ -11,44 +11,82 @@ import (
 
 type TestServer struct {
 	rf       *Raft
+	t	     *testing.T
 	wg       sync.WaitGroup
+	close	 chan interface{}
 }
 
-func NewServer(id int, peers []int) *TestServer {
+func NewServer(id int, peers []int, t *testing.T) *TestServer {
 	s := new(TestServer)
+	s.t = t
 	s.rf = NewRaft(id, peers)
+	s.close = make(chan interface{})
 	return s
 }
 
-func (s *TestServer) Start() {
-	s.wg.Add(1)
+func (ts *TestServer) Start() {
+	ts.wg.Add(1)
 	go func() {
-		defer s.wg.Done()
+		defer ts.wg.Done()
 
 		for {
-			conn, err := s.rf.listener.Accept()
+			conn, err := ts.rf.listener.Accept()
 			if err != nil {
-				log.Fatal("accept error:", err)
+				select {
+				case <-ts.close:
+					return
+				default:
+					ts.t.Fatalf("accept error: %s", err)
+				}
 			}
-			s.wg.Add(1)
+			ts.wg.Add(1)
 			go func() {
-				s.rf.server.ServeConn(conn)
-				s.wg.Done()
+				ts.rf.server.ServeConn(conn)
+				ts.wg.Done()
 			}()
 		}
 	}()
 }
 
-func (s *TestServer) Connect(id int, addr net.Addr) {
-	s.rf.mu.Lock()
-	defer s.rf.mu.Unlock()
-	if s.rf.clients[id] == nil {
+func (ts *TestServer) Connect(id int, addr net.Addr) {
+	ts.rf.mu.Lock()
+	defer ts.rf.mu.Unlock()
+	if ts.rf.clients[id] == nil {
 		client, err := rpc.Dial(addr.Network(), addr.String())
 		if err != nil {
-			log.Fatalf("unable to connect peers: %s", err.Error())
+			ts.t.Fatalf("[%v] unable to connect peers: %s", ts.rf.me, err.Error())
 		}
-		s.rf.clients[id] = client
+		ts.rf.clients[id] = client
 	}
+}
+
+// Disconnect disconnects RPC client with id `id`
+func (ts *TestServer) Disconnect(id int) {
+	ts.rf.mu.Lock()
+	defer ts.rf.mu.Unlock()
+	if ts.rf.clients[id] != nil {
+		if err := ts.rf.clients[id].Close(); err != nil {
+			ts.t.Fatalf("[%v] unable to close connection to %d: %s", ts.rf.me, id, err.Error())
+		}
+		ts.rf.clients[id] = nil
+	}
+}
+
+// DisconnectPeers disconnects all RPC clients
+func (ts *TestServer) DisconnectPeers() {
+	for id := range ts.rf.clients {
+		ts.Disconnect(id)
+	}
+}
+
+// Close closes the RPC server
+func (ts *TestServer) Close() {
+	ts.rf.Kill()
+	close(ts.close)
+	if err := ts.rf.listener.Close(); err != nil {
+		ts.t.Fatalf("[%v] unable to close listener: %s", ts.rf.me, err.Error())
+	}
+	ts.wg.Wait() // wait for server to stop serving
 }
 
 type TestCluster struct {
@@ -56,7 +94,7 @@ type TestCluster struct {
 	t *testing.T
 }
 
-func NewTestCluster(t *testing.T, n int) *TestCluster {
+func NewTestCluster(n int, t *testing.T) *TestCluster {
 	servers := make([]*TestServer, n)
 
 	for i := 0; i < n; i++ {
@@ -66,8 +104,7 @@ func NewTestCluster(t *testing.T, n int) *TestCluster {
 				peers = append(peers, p)
 			}
 		}
-
-		servers[i] = NewServer(i, peers)
+		servers[i] = NewServer(i, peers, t)
 		servers[i].Start()
 	}
 
@@ -81,9 +118,45 @@ func NewTestCluster(t *testing.T, n int) *TestCluster {
 			}
 		}
 	}
+
 	return &TestCluster{cluster: servers, t: t}
 }
 
+// DisconnectCluster kills the connection between each Raft server
+func (tc *TestCluster) DisconnectCluster() {
+	log.Printf("[c] closing connectings amongst Raft server")
+	for _, ts := range tc.cluster {
+		ts.DisconnectPeers()
+	}
+	for _, ts := range tc.cluster {
+		ts.Close()
+	}
+}
+
+// DisconnectServer disconnects a Raft server from its peers
+func (tc *TestCluster) DisconnectServer(id int) {
+	log.Printf("[c] disconnecting %d to rest of the cluster", id)
+	tc.cluster[id].DisconnectPeers()
+	for i := 0; i < len(tc.cluster); i++ {
+		if i == id {
+			continue
+		}
+		tc.cluster[i].Disconnect(id)
+	}
+}
+
+// ConnectServer connects a Raft server to all of its peers
+func (tc *TestCluster) ConnectServer(id int) {
+	log.Printf("[c] connecting %d to rest of the cluster", id)
+	for j := 0; j < len(tc.cluster); j++ {
+		if j != id {
+			tc.cluster[id].Connect(j, tc.cluster[j].rf.listener.Addr())
+			tc.cluster[j].Connect(id, tc.cluster[id].rf.listener.Addr())
+		}
+	}
+}
+
+// ==== TESTING METHODS ====
 
 // FindLeader looks for and returns the current leader
 // If no leader or more leaders are found, the test will fail
