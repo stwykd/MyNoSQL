751c6eee9ee43f54b57feb1f4c7ab9ecb7bfb0ad
stwykd <5012027+stwykd@users.noreply.github.com>
2020-06-19 12:03:45 +0100

    refactor tests

--- src/raft/raft_test.go
+++ src/raft/raft_test.go
@@ -6,26 +6,28 @@ import (
 )
 
 
+// === LEADER ELECTION ===
+
 func TestElection(t *testing.T) {
 	// setup new test cluster with 3 machines
 	tc := NewTestCluster(3, t)
 	// prevent goroutines from affecting other tests
 	defer tc.KillCluster()
 
-	FindLeader(tc, t)
+	tc.FindLeader(t)
 }
 
 func TestLeaderDown(t *testing.T) {
 	tc := NewTestCluster(3, t)
 	defer tc.KillCluster()
 
-	leader, term := FindLeader(tc, t)
+	leader, term := tc.FindLeader(t)
 
 	tc.cluster[leader].DisconnectPeers()
 
 	time.Sleep(ServerSlack)
 
-	newLeader, newTerm := FindLeader(tc, t)
+	newLeader, newTerm := tc.FindLeader(t)
 	if newLeader == leader {
 		t.Errorf("leader didn't change after disconencting leader")
 	}
@@ -37,15 +39,15 @@ func TestLeaderDown(t *testing.T) {
 func TestReconnectLeader(t *testing.T) {
 	tc := NewTestCluster(5, t)
 	defer tc.KillCluster()
-	leader, _ := FindLeader(tc, t)
+	leader, _ := tc.FindLeader(t)
 
 	tc.DisconnectServer(leader)
 	time.Sleep(ServerSlack)
-	newLeader, newTerm := FindLeader(tc, t)
+	newLeader, newTerm := tc.FindLeader(t)
 	tc.ConnectServer(leader)
 	time.Sleep(ServerSlack)
 
-	newLeader_, newTerm_ := FindLeader(tc, t)
+	newLeader_, newTerm_ := tc.FindLeader(t)
 
 	if newLeader != newLeader_ {
 		t.Errorf("leader changed from %d to %d", newLeader, newLeader_)
@@ -55,11 +57,11 @@ func TestReconnectLeader(t *testing.T) {
 	}
 }
 
-func TestNoQuorum(t *testing.T) {
+func TestElectionNoMajority(t *testing.T) {
 	tc := NewTestCluster(3, t)
 	defer tc.KillCluster()
 
-	leader, _ := FindLeader(tc, t)
+	leader, _ := tc.FindLeader(t)
 
 	// remove 2/3 servers
 	server := (leader+1)%len(tc.cluster)
@@ -70,7 +72,7 @@ func TestNoQuorum(t *testing.T) {
 
 	NoQuorum(tc, t)
 	tc.ConnectServer(server)
-	FindLeader(tc, t)
+	tc.FindLeader(t)
 }
 
 func TestRestartCluster(t *testing.T) {
@@ -82,15 +84,15 @@ func TestRestartCluster(t *testing.T) {
 	NoQuorum(tc, t)
 
 	tc.ConnectCluster()
-	FindLeader(tc, t)
+	tc.FindLeader(t)
 }
 
 func TestKeepResettingLeader(t *testing.T) {
 	tc := NewTestCluster(3, t)
 	defer tc.KillCluster()
 
-	for cycle := 0; cycle < 5; cycle++ {
-		leaderId, _ := FindLeader(tc, t)
+	for i := 0; i < 5; i++ {
+		leaderId, _ := tc.FindLeader(t)
 
 		tc.DisconnectServer(leaderId)
 		otherId := (leaderId + 1) % 3
@@ -109,7 +111,7 @@ func TestReconnectFollower(t *testing.T) {
 	tc := NewTestCluster(3, t)
 	defer tc.KillCluster()
 
-	leader, term := FindLeader(tc, t)
+	leader, term := tc.FindLeader(t)
 
 	other := (leader + 1) % 3
 	tc.DisconnectServer(other)
