b0c9e96f5c43392b44d2afccbad121a04882e517
stwykd <5012027+stwykd@users.noreply.github.com>
2020-06-23 13:56:32 +0100

    Add methods to restore and persist Raft state

--- src/raft/persist.go
+++ src/raft/persist.go
@@ -11,3 +11,48 @@ type Storage interface {
 	Get(key string) ([]byte, bool)
 	Ready() bool
 }
+
+
+func (rf *Raft) restore() {
+	if encodedTerm, found := rf.storage.Get("currentTerm"); found {
+		if err := gob.NewDecoder(bytes.NewBuffer(encodedTerm)).Decode(&rf.currentTerm); err != nil {
+			log.Fatalf("[%v] %s", rf.me, err.Error())
+		}
+	} else {
+		log.Fatalf("[%v] error restoring currentTerm", rf.me)
+	}
+	if encodedVoteFor, found := rf.storage.Get("votedFor"); found {
+		if err := gob.NewDecoder(bytes.NewBuffer(encodedVoteFor)).Decode(&rf.votedFor); err != nil {
+			log.Fatalf("[%v] %s", rf.me, err.Error())
+		}
+	} else {
+		log.Fatalf("[%v] error restoring votedFor", rf.me)
+	}
+	if encodedLog, found := rf.storage.Get("log"); found {
+		if err := gob.NewDecoder(bytes.NewBuffer(encodedLog)).Decode(&rf.log); err != nil {
+			log.Fatalf("[%v] %s", rf.me, err.Error())
+		}
+	} else {
+		log.Fatalf("[%v] error restoring log", rf.me)
+	}
+}
+
+func (rf *Raft) persist() {
+	var encodedTerm bytes.Buffer
+	if err := gob.NewEncoder(&encodedTerm).Encode(rf.currentTerm); err != nil {
+		log.Fatalf("[%v] %s", rf.me, err.Error())
+	}
+	rf.storage.Set("currentTerm", encodedTerm.Bytes())
+
+	var encodedVotedFor bytes.Buffer
+	if err := gob.NewEncoder(&encodedVotedFor).Encode(rf.votedFor); err != nil {
+		log.Fatalf("[%v] %s", rf.me, err.Error())
+	}
+	rf.storage.Set("votedFor", encodedVotedFor.Bytes())
+
+	var encodedLog bytes.Buffer
+	if err := gob.NewEncoder(&encodedLog).Encode(rf.log); err != nil {
+		log.Fatalf("[%v] %s", rf.me, err.Error())
+	}
+	rf.storage.Set("log", encodedLog.Bytes())
+}
